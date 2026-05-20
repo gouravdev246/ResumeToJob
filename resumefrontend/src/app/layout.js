cd20901c22c51e0596d533d682bed02ff2e31adb
Gourav <132838923+gouravdev246@users.noreply.github.com>
2026-05-21 00:44:25 +0530

    feat: initialize project structure with Next.js, Vercel Analytics, and base configuration

--- resumefrontend/src/app/layout.js
+++ resumefrontend/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from '@vercel/analytics/react';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body suppressHydrationWarning className="min-h-full flex flex-col">{children}</body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col">{children} <Analytics /> </body>
     </html>
   );
 }
--- resumefrontend/src/config.js
+++ resumefrontend/src/config.js
@@ -1,1 +1,5 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:5001';
+let baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:5001';
+if (baseUrl.endsWith('/')) {
+  baseUrl = baseUrl.slice(0, -1);
+}
+export const BASE_URL = baseUrl;
